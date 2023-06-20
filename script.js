be5fcd3f4a2c9f175370012dcfe9bee10b607126
Sahil patil <128183760+SP1911@users.noreply.github.com>
2023-06-20 19:23:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-const decrementBtn = document.getElementById("decrementBtn");
-const incrementBtn = document.getElementById("incrementBtn");
-const resetBtn = document.getElementById("resetBtn");
-const displayValue = document.getElementById("displayValue");
+const boxElement = document.getElementById("box");
 
-// for decrement button click
-decrementBtn.addEventListener("click", () => {
-  const value = Number(displayValue.innerText);
-  if (value > 0) {
-    displayValue.innerText = value - 1;
-  } else {
-    alert("Negative value not allowed");
-  }
-});
-
-// for increment button click
-incrementBtn.addEventListener("click", () => {
-  const value = Number(displayValue.innerText);
-  if (value >= 10) {
-    alert("10+ values are not allowed");
-  } else {
-    displayValue.innerText = value + 1;
-  }
-});
-
-// for reset button click
-resetBtn.addEventListener("click", () => {
-  displayValue.innerText = 0;
+// add a click event listener to the element
+boxElement.addEventListener("click", function () {
+  alert("Thank You");
 });
