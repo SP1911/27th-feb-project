b0fb16e9e06e50d0628ea3b185e66c069112fc7b
Sahil patil <128183760+SP1911@users.noreply.github.com>
2023-06-20 19:33:51 +0530

    Add files via upload

---
+++ Event list project/03 Mouse over/script.js
@@ -0,0 +1,8 @@
+const boxElement = document.getElementById("box");
+
+// add a mouseover event listener to the element
+boxElement.addEventListener("mouseover", function () {
+  alert(
+    "Your mouse is just over me"
+  );
+});
---
+++ Event list project/04 Mouse out/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box");
+
+// add a mouseout event listener to the element
+boxElement.addEventListener("mouseout", function () {
+  alert("Your mouse is just out");
+});
